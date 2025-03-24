79e81bebf62738aa8767c1ed4544cb22153e06c5
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-24 11:26:01 +0530

    dropdown made

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,41 +13,43 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="navbar bg-base-100 flex justify-between mt-2 mx-10">
+      <div className="navbar bg-base-100 flex mt-2 px-4 justify-between border-box ">
         <div className="flex flex-row gap-6">
           <img
             src="/luffy.png"
             className="rounded-full size-16"
             alt="onepieceimg"
           />
-          <div className="flex-1">
+          <div className="flex-1 flex  justify-start items-center">
             <a className=" text-[26px] text-primary">One Piece Theories</a>
           </div>
         </div>
 
-        <div className="flex-none gap-2 mr-20">
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <CircleUserRound className="size-full text-accent" />
-              </div>
+        <div className="dropdown dropdown-end pr-9">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <CircleUserRound className="size-full text-accent" />
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] w-20 shadow"
-            >
-              <li className="text-secondary text-2xl">
-                <a onClick={handleLogout}>
-                  <LogOut className=" text-primary" />
-                  Logout
-                </a>
-              </li>
-            </ul>
           </div>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box shadow absolute right-0 w-28 mt-2 z-50 translate-x-[-30px]"
+          >
+            <li className="text-secondar ">
+              <button
+                onClick={handleLogout}
+                className="flex items-center gap-2"
+              >
+                <LogOut className="text-primary" />
+                <p className="text-[18px] text-primary mb-1">Logout</p>
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
     </>
